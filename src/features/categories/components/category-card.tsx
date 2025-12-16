06dcb5b7b26699afacfa4f9e5aed18c0cae14784
yarinmzrc <yarinmzrc@gmail.com>
2025-12-16 18:39:25 +0200

    update category card

--- src/features/categories/components/category-card.tsx
+++ src/features/categories/components/category-card.tsx
@@ -6,28 +6,18 @@ import Link from "next/link"
 
 export function CategoryCard({ category }: { category: Category }) {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-lg">
-          <Button asChild variant="outline">
-            <Link href={paths.app.category.getHref(category.id)}>
-              {category.name}
-            </Link>
-          </Button>
-        </CardTitle>
-      </CardHeader>
-    </Card>
+    <Button asChild variant="outline">
+      <Link href={paths.app.category.getHref(category.id)}>
+        {category.name}
+      </Link>
+    </Button>
   )
 }
 
 export function CategoryCardSkeleton() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-lg">
-          <div className="h-6 animate-pulse rounded-full bg-gray-300"></div>
-        </CardTitle>
-      </CardHeader>
-    </Card>
+    <Button asChild variant="outline">
+      <div className="h-6 animate-pulse rounded-full bg-gray-300"></div>
+    </Button>
   )
 }
