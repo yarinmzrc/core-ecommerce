5339608a2a7e47997bec2e5499dc8525b4c3cbad
yarinmzrc <yarinmzrc@gmail.com>
2025-12-25 21:14:28 +0200

    format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from "eslint-plugin-storybook"
 
 import nextVitals from "eslint-config-next/core-web-vitals"
 import { defineConfig, globalIgnores } from "eslint/config"
