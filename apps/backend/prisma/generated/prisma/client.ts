4ad67b11d222f4c9aa66cea7d3ab309954b19841
yarinmzrc <yarinmzrc@gmail.com>
2026-01-09 18:51:31 +0200

    new prisma client

--- apps/backend/prisma/generated/prisma/client.ts
+++ apps/backend/prisma/generated/prisma/client.ts
@@ -44,6 +44,14 @@ export { Prisma }
 path.join(__dirname, "libquery_engine-darwin.dylib.node")
 path.join(process.cwd(), "prisma/generated/prisma/libquery_engine-darwin.dylib.node")
 
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-linux-musl.so.node")
+path.join(process.cwd(), "prisma/generated/prisma/libquery_engine-linux-musl.so.node")
+
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-rhel-openssl-3.0.x.so.node")
+path.join(process.cwd(), "prisma/generated/prisma/libquery_engine-rhel-openssl-3.0.x.so.node")
+
 /**
  * Model Image
  * 
